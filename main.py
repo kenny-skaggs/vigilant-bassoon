ff6090cda78ec2db14c90ace5676490cda44e94f
kenny-skaggs <skaggskd88@gmail.com>
2023-07-01 00:50:46 -0700

    templating

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
 
-@app.get('/')
-def get_root():
-    return "Hello World"
+@app.get('/hello')
+def get_root(request: Request):
+    return templates.TemplateResponse('home.html', {"request": request, "message": "hello temlpate"})
